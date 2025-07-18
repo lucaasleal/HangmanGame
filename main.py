9862644615732b4b8e894ea3dbada1327f79338d
lucaasleal <lucasfaleal@gmail.com>
2025-07-18 15:00:47 -0300

    Timer de jogo adicionado

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from machine import Pin, SoftI2C
 from pico_i2c_lcd import I2cLcd
-from time import sleep
+from time import sleep, ticks_ms, ticks_diff #Biblioteca para cálculo do tempo
 from random import choice
 from temas import *  # Importa o dicionário de temas e palavras
 
 #configuração do LCD 20x4 via I2C
-i2c = SoftI2C(sda=Pin(4), scl=Pin(5), freq=400000)
+i2c = SoftI2C(sda=Pin(0), scl=Pin(1), freq=400000)
 lcd = I2cLcd(i2c, 0x27, 4, 20)
+ultimo_segundo_som = -1
 
 #botoes
-bot_direita = Pin(12, Pin.IN, Pin.PULL_UP)
-bot_ok = Pin(11, Pin.IN, Pin.PULL_UP)
-bot_esquerda = Pin(10, Pin.IN, Pin.PULL_UP)
+bot_direita = Pin(8, Pin.IN, Pin.PULL_UP)
+bot_ok = Pin(7, Pin.IN, Pin.PULL_UP)
+bot_esquerda = Pin(6, Pin.IN, Pin.PULL_UP)
 
 # Estados anteriores dos botões
 estado_ant_direita = 1
@@ -26,7 +27,15 @@ indice_letra = 0
 letras_usadas = []
 erros = 0
 max_erros = 6
+tempo_total = 90000
+tempo_inicio = 0
 estado_ant_direita= estado_ant_esquerda= estado_ant_ok = False
+
+#Deboucing timers
+ultimo_tempo_direita = 0
+ultimo_tempo_esquerda = 0
+ultimo_tempo_ok = 0
+tempo_debounce = 150 
 
 #Custom chars usados
 seta_esquerda = bytearray([0b00010, 0b00110, 0b01110, 0b11110, 0b01110, 0b00110, 0b00010, 0b00000])
@@ -49,11 +58,12 @@ lcd.custom_char(7, leg_right)
 
 #inicializacao da rodada
 def init():
-    global letras_descobertas, letras_usadas, indice_letra, erros, alfabeto_game, palavra
+    global letras_descobertas, letras_usadas, indice_letra, erros, alfabeto_game, palavra, tempo_inicio
     letras_descobertas = []
     letras_usadas = ["*"]
     indice_letra = 0
     erros = 0
+    
     alfabeto_game = alfabeto.copy()
     
     lcd.clear()
@@ -76,6 +86,7 @@ def init():
         letras_descobertas.append('_' if letra != ' ' else ' ') 
     
     atualizar_display()
+    tempo_inicio = ticks_ms()
 
 #Selecao de tema pelo usuario
 def selecionar_tema():
@@ -140,7 +151,7 @@ def atualizar_display():
     lcd.move_to(0, 0); lcd.putstr("+-+")
     lcd.move_to(0, 1); lcd.putstr("|")
     lcd.move_to(0, 2); lcd.putstr("|")
-    lcd.move_to(0, 3); lcd.putstr("|")
+    lcd.move_to(0, 3); lcd.putstr("|")  
     mostrar_frase_lcd(palavra)
     mostrar_letras_vizinhas()
 
@@ -163,34 +174,43 @@ while True:
 
     # Direita pressionada
     if estado_ant_direita and not valor_direita:
-        indice_letra = (indice_letra + 1) % len(alfabeto_game)
-        atualizou = True
+        if ticks_diff(ticks_ms(), ultimo_tempo_direita) > tempo_debounce:
+            indice_letra = (indice_letra + 1) % len(alfabeto_game)
+            atualizou = True
+            ultimo_tempo_direita = ticks_ms()
 
     # Esquerda pressionada
     if estado_ant_esquerda and not valor_esquerda:
-        indice_letra = (indice_letra - 1) % len(alfabeto_game)
-        atualizou = True
+        if ticks_diff(ticks_ms(), ultimo_tempo_esquerda) > tempo_debounce:
+            indice_letra = (indice_letra - 1) % len(alfabeto_game)
+            atualizou = True
+            ultimo_tempo_esquerda = ticks_ms()
+
 
     # OK pressionado
     if estado_ant_ok and not valor_ok:
-        letra = alfabeto_game[indice_letra]
-        alfabeto_game[indice_letra] = "*"
+        if ticks_diff(ticks_ms(), ultimo_tempo_ok) > tempo_debounce:
+            letra = alfabeto_game[indice_letra]
+            alfabeto_game[indice_letra] = "*"
 
-        if letra not in letras_usadas:
-            letras_usadas.append(letra)
-            if letra in palavra:
-                for i, l in enumerate(palavra):
-                    if l == letra:
-                        letras_descobertas[i] = letra
+            if letra not in letras_usadas:
+                letras_usadas.append(letra)
+                if letra in palavra:
+                    for i, l in enumerate(palavra):
+                        if l == letra:
+                            letras_descobertas[i] = letra
+                else:
+                    erros += 1
+                atualizou = True
             else:
-                erros += 1
-            atualizou = True
-        else:
-            lcd.clear()
-            lcd.move_to(5, 1)
-            lcd.putstr("Ja usada!")
-            sleep(1)
-            atualizou = True
+                lcd.clear()
+                lcd.move_to(5, 1)
+                lcd.putstr("Ja usada!")
+                sleep(1)
+                atualizou = True
+
+            ultimo_tempo_ok = ticks_ms()
+
 
     # Atualiza estados anteriores
     estado_ant_direita = valor_direita
@@ -200,17 +220,18 @@ while True:
     # Verifica fim de jogo
     if "_" not in letras_descobertas:
         atualizar_display()
-        sleep(2)
+        sleep(0.5)
         lcd.clear()
         lcd.move_to(4, 0); lcd.putstr("Voce venceu!")
         lcd.move_to(0, 1); lcd.putstr("O item era:")
         lcd.move_to(0, 2); lcd.putstr(f'"{palavra}"')
         sleep(3)
         init()
-
-    if erros >= max_erros:
+    
+    tempo_atual = ticks_ms()
+    if erros >= max_erros or (tempo_atual-tempo_inicio)>=tempo_total:
         atualizar_display()
-        sleep(2)
+        sleep(0.5)
         lcd.clear()
         lcd.move_to(4, 0); lcd.putstr("Game Over :(")
         lcd.move_to(0, 1); lcd.putstr("O item era:")
@@ -220,5 +241,10 @@ while True:
 
     if atualizou:
         atualizar_display()
+        
+    lcd.move_to(4, 3); lcd.putstr(f"{max((tempo_total - (tempo_atual - tempo_inicio)) // 1000, 0):02}s") #Atualizacao do tempo
+    seg_restantes = max((tempo_total - (tempo_atual - tempo_inicio)) // 1000, 0)
 
-    sleep(0.05)  # Delay pra dar um efeito
+    if seg_restantes <= 10 and seg_restantes != ultimo_segundo_som:
+        music.buzzer.play("c4", 0.2)
+        ultimo_segundo_som = seg_restantes
